e360148c751315a4f6140a21f8aa401a5e98a640
MathiasPetak <wi18b085@technikum-wien.at>
2021-01-21 16:23:11 +0100

    Projektdateien hinzufügen.

---
+++ EXAM210121/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to calculate
+	As a student
+	I want to solve trigonometric operations
+
+@mytag
+Scenario: Calculate sinus
+	Given the angle is Pi
+	When sinus of the angle is calculated
+	Then the result should be 0
+
+	Scenario: Calculate cosinus
+	Given the angle is Pi
+	When cosinus of the angle is calculated
+	Then the result should be -1
+
+	Scenario: Calculate tangens
+	Given the angle is Pi
+	When tangens of the angle is calculated
+	Then the result should be 0
---
+++ EXAM210121/SpecFlowFeature1Steps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace EXAM210121
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private double angle;
+        private double result;
+
+        [Given(@"the angle is Pi")]
+        public void GivenTheAngleIsPi()
+        {
+            angle = Math.PI;
+        }
+        
+        [When(@"sinus of the angle is calculated")]
+        public void WhenSinusOfTheAngleIsCalculated()
+        {
+            result = Math.Sin(angle);
+        }
+        
+        [When(@"cosinus of the angle is calculated")]
+        public void WhenCosinusOfTheAngleIsCalculated()
+        {
+            result = Math.Cos(angle);
+        }
+        
+        [When(@"tangens of the angle is calculated")]
+        public void WhenTangensOfTheAngleIsCalculated()
+        {
+            result = Math.Tan(angle);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, result, 5);
+        }
+    }
+}
